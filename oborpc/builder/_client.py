f0349fb79e68230c64aff3aa3f506d33031ffbad
danangjoyoo <agus.danangjoyo.blog@gmail.com>
2025-01-10 00:54:33 +0700

    pylint

--- oborpc/builder/_client.py
+++ oborpc/builder/_client.py
@@ -82,7 +82,7 @@ class ClientBuilder:
         url_prefix: str,
         timeout: float = None,
         retry: int = None
-    ): # pylint: disable=too-many-arguments
+    ): # pylint: disable=too-many-arguments,too-many-positional-arguments
         """
         create remote caller
         """
@@ -132,7 +132,7 @@ class ClientBuilder:
         url_prefix: str,
         timeout: float = None,
         retry: int = None
-    ): # pylint: disable=too-many-arguments
+    ): # pylint: disable=too-many-arguments,too-many-positional-arguments
         """
         create async remote caller
         """
@@ -212,6 +212,7 @@ class ClientBuilder:
         method: Callable
     ):
         """
+        Extract pydantic model
         """
         if not class_name in self.model_maps:
             self.model_maps[class_name] = {}
@@ -237,6 +238,7 @@ class ClientBuilder:
         response: Dict[str, Any]
     ) -> Dict[str, Any]:
         """
+        Convert Pydantic Model Response
         """
         model, return_annotation = self.model_maps[class_name][method_name]
         try:
